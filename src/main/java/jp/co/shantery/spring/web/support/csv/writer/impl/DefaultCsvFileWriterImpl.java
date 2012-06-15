55410d56dc1a58519f16336eee58507ff7dd0f9e
m-namiki <masa1824@gmail.com>
2012-06-15 10:46:31 +0900

    writeメソッドを実装しました。

--- src/main/java/jp/co/shantery/spring/web/support/csv/writer/impl/DefaultCsvFileWriterImpl.java
+++ src/main/java/jp/co/shantery/spring/web/support/csv/writer/impl/DefaultCsvFileWriterImpl.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 import jp.co.shantery.spring.web.support.annotation.CsvColumn;
 import jp.co.shantery.spring.web.support.csv.writer.CsvFileWriter;
+import jp.co.shantery.spring.web.support.exception.IORuntimeException;
+import jp.co.shantery.spring.web.support.util.SWFieldUtils;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.BeanWrapper;
@@ -33,6 +35,10 @@ public class DefaultCsvFileWriterImpl implements CsvFileWriter {
 	/** 書き出し対象ファイルです。 */
 	private File file;
 
+	private BufferedWriter writer;
+
+	private List<String> orderList;
+
 	@Override
 	public void setFile(File file) {
 		this.file = file;
@@ -40,14 +46,32 @@ public class DefaultCsvFileWriterImpl implements CsvFileWriter {
 
 	@Override
 	public void write(Object obj) {
-		// TODO Auto-generated method stub
+		try {
+			if (null == writer) {
+				writer = new BufferedWriter(new FileWriter(file));
+			}
+			if (null == orderList) {
+				orderList = new ArrayList<>();
+				setOutputOrder(obj);
+			}
+
+			StringBuilder buf = new StringBuilder();
+			for (String columnName : orderList) {
+				buf.append(getValue(obj, columnName) + ",");
+			}
+			buf.substring(0, buf.length() - 1);
+			writer.append(buf.toString());
+			writer.flush();
+
+		} catch (IOException e) {
+			throw new IORuntimeException(e);
+		}
 
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-
+		IOUtils.closeQuietly(writer);
 	}
 
 	@Override
@@ -55,35 +79,39 @@ public class DefaultCsvFileWriterImpl implements CsvFileWriter {
 		if (null == file) {
 			throw new IllegalStateException("file isn't specified.");
 		}
-
-		BufferedWriter writer = null;
 		try {
-			writer = new BufferedWriter(new FileWriter(file));
-			List<String> orderList = new ArrayList<>();
 			for (Object obj : collection) {
-				// TODO 一回だけやって、後はキャッシュしておく
-				BeanWrapper beanWrapper = PropertyAccessorFactory
-						.forBeanPropertyAccess(obj);
-				PropertyDescriptor[] propDescs = beanWrapper
-						.getPropertyDescriptors();
-				for (PropertyDescriptor propDesc : propDescs) {
-					TypeDescriptor typeDesc = beanWrapper
-							.getPropertyTypeDescriptor(propDesc.getName());
-					CsvColumn col = (CsvColumn) typeDesc
-							.getAnnotation(CsvColumn.class);
-					if (null != col) {
-						orderList.add(col.index(), propDesc.getName());
-					}
-				}
+				write(obj);
 			}
-
-			for (String propName : orderList) {
-				System.out.println(propName);
-			}
-		} catch (IOException e) {
-			IOUtils.closeQuietly(writer);
+		} finally {
+			close();
 		}
-
 	}
 
+	/**
+	 * 指定されたオブジェクトから出力順序を決めます。
+	 * 
+	 * @param obj
+	 *            出力対象オブジェクト
+	 */
+	private void setOutputOrder(Object obj) {
+		// 出力順序の設定
+		BeanWrapper beanWrapper = PropertyAccessorFactory
+				.forBeanPropertyAccess(obj);
+		PropertyDescriptor[] propDescs = beanWrapper.getPropertyDescriptors();
+		for (PropertyDescriptor propDesc : propDescs) {
+			TypeDescriptor typeDesc = beanWrapper
+					.getPropertyTypeDescriptor(propDesc.getName());
+			CsvColumn col = (CsvColumn) typeDesc.getAnnotation(CsvColumn.class);
+			if (null != col) {
+				orderList.add(col.index(), propDesc.getName());
+			}
+		}
+	}
+
+	private String getValue(Object obj, String columnName) {
+		return SWFieldUtils.get(
+				SWFieldUtils.getField(obj.getClass(), columnName), obj)
+				.toString();
+	}
 }
